7d083dcf0e7eafba36604af45dac87745f50ef62
Matthew Carr <76042279+matthew-a-carr@users.noreply.github.com>
2024-11-23 15:32:57 +0000

    Update site

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,189 @@
+import { FaEnvelope, FaGithub, FaJava, FaLinkedin } from 'react-icons/fa';
+import {
+  SiSpringboot,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiDocker,
+  SiGit,
+  SiKotlin,
+  SiAmazon,
+  SiNodedotjs,
+  SiTerraform,
+  SiMicrosoftazure,
+  SiPostgresql,
+  SiMysql,
+  SiMongodb,
+} from 'react-icons/si';
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-4xl mx-auto">
-        <div className="bg-white rounded-lg shadow-md p-8 mb-8">
-          <div className="border-l-4 border-gray-800 pl-4 mb-8">
-            <h1 className="text-3xl font-bold text-gray-800 mb-2">
-              Matthew Carr
-            </h1>
-            <h2 className="text-xl text-gray-600">Software Engineer</h2>
+      <div className="max-w-6xl mx-auto space-y-12">
+        {/* Header Section */}
+        <div className="bg-white rounded-lg shadow-md p-8">
+          <div className="flex items-center space-x-6 mb-6">
+            <img
+              src="https://avatars.githubusercontent.com/u/76042279?v=4"
+              alt="Matthew Carr"
+              className="w-20 h-20 rounded-full object-center shadow-md"
+            />
+            <div>
+              <h1 className="text-3xl font-bold text-gray-800 mb-1">
+                Matthew Carr
+              </h1>
+              <h2 className="text-lg text-gray-600">
+                Senior Software Engineer
+              </h2>
+            </div>
           </div>
 
-          <p className="text-gray-600 mb-6"></p>
+          <p className="text-gray-800">
+            Software engineer and professional tea drinker.
+          </p>
+        </div>
 
-          <div className="flex space-x-4">
-            <a
-              href="https://github.com/matthew-a-carr"
-              target="_blank"
-              aria-label="GitHub"
-            >
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-              >
-                <title>GitHub</title>
-                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-              </svg>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/matthew-carr-17012284"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-              >
-                <title>LinkedIn</title>
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-              </svg>
-            </a>
-            <a href="mailto:carr.matty@gmail.com" aria-label="Email">
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-              >
-                <title>Gmail</title>
-                <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
-              </svg>
-            </a>
+        {/* Current Job Section */}
+        <div className="bg-white rounded-lg shadow-md p-8 flex items-center space-x-6">
+          <img
+            src="https://via.placeholder.com/100" // Replace with Boclips' logo
+            alt="Boclips"
+            className="w-24 h-24"
+          />
+          <div>
+            <h3 className="text-2xl font-semibold text-gray-800">
+              Senior Software Engineer at Boclips
+            </h3>
+            <p className="text-gray-600">
+              Developing educational video platforms that connect educators with
+              engaging content.
+            </p>
           </div>
         </div>
 
+        {/* Skills Section */}
         <div className="grid md:grid-cols-2 gap-6">
           <div className="bg-white rounded-lg shadow-md p-8">
             <h3 className="text-xl font-semibold text-gray-800 mb-4 flex items-center">
               <span className="mr-2">⚡</span>
               Technical Skills
             </h3>
-            <div className="flex flex-wrap gap-2">
+            <div className="flex flex-wrap gap-4">
               {[
-                'Java',
-                'Spring Boot',
-                'JavaScript',
-                'TypeScript',
-                'React',
-                'Node.js',
-                'AWS',
-                'Docker',
-                'Git',
+                {
+                  name: 'Kotlin',
+                  icon: <SiKotlin className="text-blue-700 w-6 h-6" />,
+                },
+                {
+                  name: 'Java',
+                  icon: <FaJava className="text-orange-700 w-6 h-6" />,
+                },
+                {
+                  name: 'Spring Boot',
+                  icon: <SiSpringboot className="text-green-600 w-6 h-6" />,
+                },
+                {
+                  name: 'PostgreSQL',
+                  icon: <SiPostgresql className="text-blue-600 w-6 h-6" />,
+                },
+                {
+                  name: 'MySQL',
+                  icon: <SiMysql className="text-orange-600 w-6 h-6" />,
+                },
+                {
+                  name: 'MongoDB',
+                  icon: <SiMongodb className="text-green-600 w-6 h-6" />,
+                },
+                {
+                  name: 'Terraform',
+                  icon: <SiTerraform className="text-purple-700 w-6 h-6" />,
+                },
+                {
+                  name: 'JavaScript',
+                  icon: <SiJavascript className="text-yellow-500 w-6 h-6" />,
+                },
+                {
+                  name: 'TypeScript',
+                  icon: <SiTypescript className="text-blue-500 w-6 h-6" />,
+                },
+                {
+                  name: 'React',
+                  icon: <SiReact className="text-cyan-500 w-6 h-6" />,
+                },
+                {
+                  name: 'Node.js',
+                  icon: <SiNodedotjs className="text-green-500 w-6 h-6" />,
+                },
+                {
+                  name: 'AWS',
+                  icon: <SiAmazon className="text-orange-500 w-6 h-6" />,
+                },
+                {
+                  name: 'Azure',
+                  icon: <SiMicrosoftazure className="text-blue-500 w-6 h-6" />,
+                },
+                {
+                  name: 'Docker',
+                  icon: <SiDocker className="text-blue-600 w-6 h-6" />,
+                },
+                {
+                  name: 'Git',
+                  icon: <SiGit className="text-red-600 w-6 h-6" />,
+                },
               ].map((skill) => (
-                <span
-                  key={skill}
-                  className="px-3 py-1 bg-gray-100 text-gray-700 rounded-full text-sm hover:bg-gray-200"
+                <div
+                  key={skill.name}
+                  className="flex items-center space-x-2 bg-gray-100 px-3 py-2 rounded-lg hover:bg-gray-200"
                 >
-                  {skill}
-                </span>
+                  {skill.icon}
+                  <span className="text-gray-700">{skill.name}</span>
+                </div>
               ))}
             </div>
           </div>
 
-          <div className="bg-white rounded-lg shadow-md p-8">
+          {/* Current Focus Section */}
+          {/* <div className="bg-white rounded-lg shadow-md p-8">
             <h3 className="text-xl font-semibold text-gray-800 mb-4 flex items-center">
               <span className="mr-2">🧑‍💻</span>
               Current Focus
             </h3>
-            <p className="text-gray-600"></p>
-          </div>
+            <p className="text-gray-600">
+              Exploring advanced distributed systems, enhancing CI/CD pipelines,
+              and mentoring junior engineers. Actively contributing to the
+              open-source community and developing innovative solutions in
+              ed-tech.
+            </p>
+          </div> */}
         </div>
       </div>
+      <footer className="bg-gray-800 py-6 mt-12">
+        <div className="max-w-6xl mx-auto flex justify-center space-x-8">
+          <a
+            href="https://github.com/matthew-a-carr"
+            target="_blank"
+            aria-label="GitHub"
+            className="text-gray-400 hover:text-white"
+          >
+            <FaGithub className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/matthew-carr-17012284"
+            target="_blank"
+            aria-label="LinkedIn"
+            className="text-gray-400 hover:text-white"
+          >
+            <FaLinkedin className="w-6 h-6" />
+          </a>
+          <a
+            href="mailto:carr.matty@gmail.com"
+            aria-label="Email"
+            className="text-gray-400 hover:text-white"
+          >
+            <FaEnvelope className="w-6 h-6" />
+          </a>
+        </div>
+      </footer>
     </main>
   );
 }
