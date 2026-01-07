2604c83ccc9fb596a941bda37130fd27b45dca45
Matthew Carr <76042279+matthew-a-carr@users.noreply.github.com>
2026-01-07 18:27:41 +0000

    test: mock next image in jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+import React from 'react';
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (
+    props: React.ImgHTMLAttributes<HTMLImageElement> & { src?: any },
+  ) => {
+    const { src, alt, ...rest } = props;
+    const resolvedSrc = typeof src === 'string' ? src : src?.src;
+
+    return React.createElement('img', { src: resolvedSrc, alt, ...rest });
+  },
+}));
