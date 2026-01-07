6d59159d6845528c9433594b577e02e9c9c31ce0
Matthew Carr <76042279+matthew-a-carr@users.noreply.github.com>
2026-01-07 18:36:14 +0000

    test: mock child components in page test

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,6 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Page from './page';
 
+jest.mock('./components/footer/Footer', () => ({
+  __esModule: true,
+  default: () => null,
+}));
+
+jest.mock('./components/technical-skills/TechnicalSkills', () => ({
+  __esModule: true,
+  default: () => null,
+}));
+
 describe('Page', () => {
   it('should render', () => {
     render(<Page />);
